23ff0b34860c2eadb574ddd43599f63288d7a405
Jake Shin <jakeshin45@gmail.com>
2020-03-31 15:25:43 -0400

    Created Mini_Scenario_1, Added 6 new animations

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+    private Animator anim;
+    private int stage;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        stage = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stage == 1)
+        {
+            anim.SetBool("IsWalking", true);
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            if (Vector3.Distance(transform.position, target.position) < 0.1)
+            {
+                stage = 2;
+                anim.SetBool("IsWalking", false);
+                anim.SetBool("IsTurning", true);
+            }
+        }
+
+        if (stage == 2)
+        {
+
+        }
+
+
+    }
+
+}
+
