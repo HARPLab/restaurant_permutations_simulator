8b9f7188b9cedf318f870a39de279990af2a77f7
Ellie Mamantov <mamantove@carleton.edu>
2020-06-22 11:57:03 -0400

    Created 2 scripts that allow a agent to walk along a path of waypoints

---
+++ Assets/Scripts/EditPathScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EditPathScript : MonoBehaviour
+{
+    public List<Transform> path_objs = new List<Transform>();
+    Transform[] theArray;
+    
+    public Transform waypoint1;
+    public Transform waypoint2;
+    public Transform waypoint3;
+    
+    private int numWaypoints = 3;
+    
+    void Start()
+    {
+        theArray = new Transform[numWaypoints];
+        theArray[0] = waypoint1;
+        theArray[1] = waypoint2;
+        theArray[2] = waypoint3;
+        path_objs.Clear();
+        
+        foreach (Transform path_obj in theArray){
+            if(path_obj != this.transform)
+            {
+                path_objs.Add(path_obj);
+            }
+        } 
+//        
+//        for(int i = 0; i < path_objs.Count; i++)
+//        {
+//            Vector3 position = path_objs[i].position;
+//            if (i > 0)
+//            {
+//                Vector3 previous = path_objs[i - 1].position;
+//                if(i>0)
+//                {
+//                    previous = path_objs[i-1].position;
+//                    Gizmos.DrawLine(previous,position);
+//                    Gizmos.DrawWireSphere(position, 0.3f);
+//                }
+//            }
+//        }
+    }
+    
+    
+}
---
+++ Assets/Scripts/MoveOnPathScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnPathScript : MonoBehaviour
+{
+
+    public EditPathScript PathToFollow;
+    
+    public int CurrentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 1.0f;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+    private Animator anim;
+    private int stage;
+    
+    Vector3 last_position;
+    Vector3 current_position;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //animation
+        anim = GetComponent<Animator>();
+        stage = 1; 
+        
+        
+        PathToFollow = GameObject.Find(pathName).GetComponent<EditPathScript>();
+        last_position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animation
+        if(stage == 1){
+            anim.SetBool("IsWalking", true);
+        }
+        
+        
+        float distance = Vector3.Distance(PathToFollow.path_objs[CurrentWayPointID].position, transform.position);
+        transform.position = Vector3.MoveTowards(transform.position, PathToFollow.path_objs[CurrentWayPointID].position, Time.deltaTime * speed);
+        
+        var rotation = Quaternion.LookRotation(PathToFollow.path_objs[CurrentWayPointID].position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        
+        if(distance <= reachDistance)
+        {
+            CurrentWayPointID++;
+        }
+        
+        if(CurrentWayPointID >= PathToFollow.path_objs.Count)
+        {
+            CurrentWayPointID = 0;
+        }
+    }
+}
